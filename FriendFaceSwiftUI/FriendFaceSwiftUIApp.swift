734121c42d669ad13859991d1394ff193bc16768
appsaf2k <appsaf2k@gmail.com>
2022-07-23 11:06:49 +0300

    Initial Commit

---
+++ FriendFaceSwiftUI/FriendFaceSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceSwiftUIApp.swift
+//  FriendFaceSwiftUI
+//
+//  Created by @andreev2k on 23.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
